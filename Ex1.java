251f7f9998689d6de66ee14a294f4fb7477201c5
geeta gohil <gitgohil@yahoo.co.in>
2023-02-14 18:36:08 +0000

    homework

---
+++ Ex1.java
@@ -0,0 +1,12 @@
+package homework;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
+public class Ex1 {
+    public static void main(String[] args){
+        System.out.println("welcome to java");
+        System.out.println("Geeta Gohil");
+        System.out.println("Email: gitgohil@yahoo.co.in");
+
+    }
+}
